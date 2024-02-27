5f3be7cdbe6c53aae289dbeca609d3ae00681cbc
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-27 12:38:01 +0200

    Added function to add multiple orders

--- test/functions.py
+++ test/functions.py
@@ -24,7 +24,7 @@ def get_user_id(username: str):
 def add_user(username):
     user = {
         "name": username,
-        "date_created": datetime.utcnow()
+        "date_created": datetime.now()
     }
     user_collection.insert_one(user)
 
@@ -36,7 +36,7 @@ def add_user_activity(username: str, hours: float):
         user_activity_collection.insert_one({
             "hours": hours,
             "user_id": user_id,
-            "date_created": datetime.utcnow()
+            "date_created": datetime.now()
         })
         print(f"Activity added for {username}.")
     else:
@@ -48,13 +48,14 @@ def add_order(
     date_completed: datetime,
     address: str, 
     order_type: str, 
+    date_ordered: datetime = datetime.now()
 ):
     user_id = get_user_id(username)
 
     if (user_id):
-        order_price = random.uniform(150, 2000)
+        order_price = round(random.uniform(150, 2000), 2)
         order = {
-            "date_ordered": datetime.utcnow(),
+            "date_ordered": date_ordered,
             "order_cancelled": order_cancelled,
             "date_completed": date_completed,
             "address": address,
@@ -68,7 +69,7 @@ def add_order(
         print(f"User with username {username} not found.")
 
 def add_order_complete(username, address, order_type):
-    add_order(username, False, datetime.utcnow(), address, order_type)
+    add_order(username, False, datetime.now(), address, order_type)
 
 def add_order_incomplete(username, address, order_type):
     add_order(username, False, None, address, order_type)
@@ -76,4 +77,25 @@ def add_order_incomplete(username, address, order_type):
 def add_order_cancelled(username, address, order_type):
     add_order(username, True, None, address, order_type)
 
-client.close()
+def add_many_complete_orders(username, address, days_before):
+    date_and_time = datetime.now()
+    start_date_and_time = date_and_time - timedelta(days=days_before)
+    order_types = ["sale", "service"]
+
+    start_date = datetime(
+        start_date_and_time.year, 
+        start_date_and_time.month, 
+        start_date_and_time.day)
+
+    for i in range(1, days_before+1):
+        order_type = order_types[random.randint(0, 1)]
+        temp_date = start_date + timedelta(days=i)
+        add_order(username, False, temp_date, address, order_type)
+    
+def clear_orders():
+    order_collection.delete_many({})
+    print("Deleted successfully")
+
+if __name__ == "__main__":
+
+    client.close()
