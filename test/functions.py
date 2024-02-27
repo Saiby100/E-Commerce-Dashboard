fa10a728a928d9c018b318cb7fb37bcebc13ca96
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-27 14:50:59 +0200

    Added function to add many cancelled orders

--- test/functions.py
+++ test/functions.py
@@ -44,11 +44,11 @@ def add_user_activity(username: str, hours: float):
     
 def add_order(
     username: str, 
-    order_cancelled: bool, 
+    date_cancelled: datetime, 
     date_completed: datetime,
     address: str, 
-    order_type: str, 
-    date_ordered: datetime = datetime.now()
+    order_type: str,
+    date_ordered = datetime.now()
 ):
     user_id = get_user_id(username)
 
@@ -56,7 +56,7 @@ def add_order(
         order_price = round(random.uniform(150, 2000), 2)
         order = {
             "date_ordered": date_ordered,
-            "order_cancelled": order_cancelled,
+            "date_cancelled": date_cancelled,
             "date_completed": date_completed,
             "address": address,
             "order_type": order_type,
@@ -69,13 +69,13 @@ def add_order(
         print(f"User with username {username} not found.")
 
 def add_order_complete(username, address, order_type):
-    add_order(username, False, datetime.now(), address, order_type)
+    add_order(username, None, datetime.now(), address, order_type)
 
 def add_order_incomplete(username, address, order_type):
-    add_order(username, False, None, address, order_type)
+    add_order(username, None, None, address, order_type)
 
-def add_order_cancelled(username, address, order_type):
-    add_order(username, True, None, address, order_type)
+def add_order_cancelled(username, date_cancelled, address, order_type):
+    add_order(username, date_cancelled, None, address, order_type)
 
 def add_many_complete_orders(username, address, days_before):
     date_and_time = datetime.now()
@@ -90,12 +90,35 @@ def add_many_complete_orders(username, address, days_before):
     for i in range(1, days_before+1):
         order_type = order_types[random.randint(0, 1)]
         temp_date = start_date + timedelta(days=i)
-        add_order(username, False, temp_date, address, order_type)
+
+        num_orders = random.randint(0, 10)
+        for j in range(0, num_orders):
+            add_order(username, None, temp_date, address, order_type)
+
+def add_many_cancelled_orders(username, address, days_before):
+    date_and_time = datetime.now()
+    start_date_and_time = date_and_time - timedelta(days=days_before)
+    order_types = ["sale", "service"]
+
+    start_date = datetime(
+        start_date_and_time.year, 
+        start_date_and_time.month, 
+        start_date_and_time.day)
+
+    for i in range(1, days_before+1):
+        order_type = order_types[random.randint(0, 1)]
+        temp_date = start_date + timedelta(days=i)
+
+        num_orders = random.randint(0, 10)
+        for j in range(0, num_orders):
+            add_order_cancelled(username, temp_date, address, order_type)
     
 def clear_orders():
     order_collection.delete_many({})
     print("Deleted successfully")
 
 if __name__ == "__main__":
+    # clear_orders()
+    add_many_complete_orders("Salahuddin", "My Address", 20)
 
     client.close()
